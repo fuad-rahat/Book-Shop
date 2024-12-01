99c4b5bcd10a43ca8b2e2cfe9e7d9161c57a015b
rahatmf <rahatmf@gmail.com>
2024-12-01 22:12:56 +0600

    fixing bug

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function main() {
     try{
         await mongoose.connect(config.db_url as string)
     app.listen(config.port, () => {
-        console.log(`Book Shop is running on port ${config.port}`)
+        console.log(`Book Shop is running on portt ${config.port}`)
       })
     }catch(error){
      console.log(error);
